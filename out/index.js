24501ac4feba392436044ce93733cabfab192e80
ViniElias <viniciuseliascr@gmail.com>
2025-04-10 12:11:38 -0300

    Testes

---
+++ out/index.js
@@ -0,0 +1,3 @@
+import { Somador } from "moduloTS";
+let soma = new Somador(1, 2);
+console.log(soma);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// import {Somador, Subtrator, Multiplicador, Divisor} from "moduloTS"
+
+// let soma = new Somador(1, 2)
+
+// console.log(soma)
