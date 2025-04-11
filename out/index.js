5e548eaedf3e577af488a6d66f64a10d5d9b2670
ViniElias <viniciuseliascr@gmail.com>
2025-04-11 08:25:46 -0300

    add import

--- out/index.js
+++ out/index.js
@@ -1,3 +1,9 @@
-import { Somador } from "moduloTS";
-let soma = new Somador(1, 2);
-console.log(soma);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_js_1 = require("ModuloTS/out/index.js");
+const a = 10;
+const b = 5;
+console.log(`Soma: ${a} + ${b} =`, new index_js_1.Somador().calcular(a, b));
+console.log(`Subtração: ${a} - ${b} =`, new index_js_1.Subtrator().calcular(a, b));
+console.log(`Multiplicação: ${a} * ${b} =`, new index_js_1.Multiplicador().calcular(a, b));
+console.log(`Divisão: ${a} / ${b} =`, new index_js_1.Divisor().calcular(a, b));
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-// import {Somador, Subtrator, Multiplicador, Divisor} from "moduloTS"
+import {Somador, Subtrator, Multiplicador, Divisor} from "ModuloTS/out/index.js"
 
-// let soma = new Somador(1, 2)
+const a = 10
+const b = 5
 
-// console.log(soma)
+console.log(`Soma: ${a} + ${b} =`, new Somador().calcular(a, b)); 
+console.log(`Subtração: ${a} - ${b} =`, new Subtrator().calcular(a, b)); 
+console.log(`Multiplicação: ${a} * ${b} =`, new Multiplicador().calcular(a, b)); 
+console.log(`Divisão: ${a} / ${b} =`, new Divisor().calcular(a, b)); 
